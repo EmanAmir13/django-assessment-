e9b3c231fe04a7b6b4f66fea2f5d569faf97d260
EmanAmir13 <eman.amir@flicken.io>
2025-02-17 11:56:33 +0500

    update the urls

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,19 @@
 from django.urls import path
 
 from api.views.authentications import RegisterView, LoginView
+from api.views.projects import ProjectListCreateView, ProjectDetailView
+from api.views.tasks import TaskListCreateView, TaskDetailView
 
 urlpatterns = [
+    # authentications
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', LoginView.as_view(), name='login'),
+
+    # projects
+    path('projects/', ProjectListCreateView.as_view(), name='project-list-create'),
+    path('projects/<int:pk>/', ProjectDetailView.as_view(), name='project-detail'),
+
+    # tasks
+    path('tasks/', TaskListCreateView.as_view(), name='task-list-create'),
+    path('tasks/<int:pk>/', TaskDetailView.as_view(), name='task-detail'),
 ]
