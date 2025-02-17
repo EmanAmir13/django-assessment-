bdf6f3fb7253233b4b2fac5f47d3a71b674a2396
EmanAmir13 <eman.amir@flicken.io>
2025-02-17 13:09:38 +0500

    add the id in readable field

--- api/serializers/task.py
+++ api/serializers/task.py
@@ -7,7 +7,7 @@ class TaskSerializer(serializers.ModelSerializer):
     class Meta:
         model = Task
         fields = ['id', 'title', 'description', 'status', 'project', 'created_at', 'updated_at']
-        read_only_fields = ['created_at', 'updated_at']
+        read_only_fields = ['id', 'created_at', 'updated_at']
 
     def validate(self, data):
         """
