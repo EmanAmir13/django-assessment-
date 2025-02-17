5f067b27369d74357a7a077776d18d1fa244cc05
EmanAmir13 <eman.amir@flicken.io>
2025-02-17 13:28:14 +0500

    remove the unrequired content

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,22 @@
 from django.urls import path
-
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 from api.views.authentications import RegisterView, LoginView
 from api.views.projects import ProjectListCreateView, ProjectDetailView
 from api.views.tasks import TaskListCreateView, TaskDetailView
+from rest_framework import permissions
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Task Management API",
+        default_version='v1',
+        description="API for managing tasks within a project",
+        contact=openapi.Contact(email="eman13.amir@gmail.com"),
+        license=openapi.License(name="MIT License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     # authentications
@@ -16,4 +30,8 @@ urlpatterns = [
     # tasks
     path('tasks/', TaskListCreateView.as_view(), name='task-list-create'),
     path('tasks/<int:pk>/', TaskDetailView.as_view(), name='task-detail'),
+
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='swagger-ui'),
+    # ReDoc UI (alternative to Swagger UI)
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='redoc'),
 ]
